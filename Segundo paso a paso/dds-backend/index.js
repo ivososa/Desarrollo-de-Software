d4a0cb0d908177b77619a729c8cf9106a91c42e0
Ivo <91078@sistemas.frc.utn.edu.ar>
2025-06-21 20:48:39 -0300

    Completando el segundo paso a paso, la parte del backend

--- Segundo paso a paso/dds-backend/index.js
+++ Segundo paso a paso/dds-backend/index.js
@@ -2,18 +2,47 @@ const express = require("express");
 
 // crear servidor
 const app = express();
+app.use(express.json()); // para poder leer json en el body
+const inicializarBase = require("./models/inicializarBase");  // inicializar base de datos
 
 // controlar ruta
 app.get("/", (req, res) => {
   res.send("Backend inicial dds-backend!");
 });
 
+const categoriasmockRouter = require("./routes/categoriasmock");
+app.use(categoriasmockRouter);
+const categoriasRouter = require("./routes/categorias");
+app.use(categoriasRouter);
+const articulosRouter = require("./routes/articulos");
+app.use(articulosRouter);
+
+// configurar servidor
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "*", // origin: 'https://dds-frontend.azurewebsites.net'
+  })
+);
+
+const seguridadRouter = require("./routes/seguridad");
+app.use(seguridadRouter);
+const usuariosRouter = require("./routes/usuarios");
+app.use(usuariosRouter);
+
+
 // levantar servidor
 const port = 3000;
 app.locals.fechaInicio = new Date();  // fecha y hora inicio de aplicacion
-app.listen(port, () => {
-    console.log(`sitio escuchando en el puerto ${port}`);
-});
+if (require.main === module) {   // si no es llamado por otro módulo, es decir, si es el módulo principal -> levantamos el servidor
+    inicializarBase().then(() => {
+     app.listen(port, () => {
+      console.log(`sitio escuchando en el puerto ${port}`);
+      });
+    });
+  }
+module.exports = app; // para testing
+  
 
 
 
